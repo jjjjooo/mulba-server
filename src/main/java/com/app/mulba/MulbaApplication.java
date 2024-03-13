6f956cd5ef804aa4c5e2ec9665cb335f98d9861c
jjjjooo <fullbster@gmail.com>
2024-03-13 09:59:45 +0900

    first commit

---
+++ src/main/java/com/app/mulba/MulbaApplication.java
@@ -0,0 +1,13 @@
+package com.app.mulba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MulbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/mulba/MulbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.mulba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
