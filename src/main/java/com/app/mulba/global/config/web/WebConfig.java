2ce4e9e1c18aa4e58b317bec3667ae7aeddee408
jjjjooo <fullbster@gmail.com>
2024-03-25 07:13:11 +0900

    [setting] #2 - Cors & .yml 설정

---
+++ src/main/java/com/app/mulba/global/config/web/WebConfig.java
@@ -0,0 +1,25 @@
+package com.app.mulba.global.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/pai/**")
+                .allowedOrigins("*", "http;//localhost:3000")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()
+                )
+                .maxAge(3600); // preflight
+    }
+}
